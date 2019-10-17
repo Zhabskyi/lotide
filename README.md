c250096417e525d8eb56555700f878c0d7180a61
Zhabskyi <poltavagaz@gmail.com>
2019-10-17 20:13:46 +0000

    Added findKey.js file

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### The Lotide project is a modular collection of functions that operate on data, namely Arrays and Objects. In creating these functions, we will become more comfortable with FOCAL as well as writing modular code and Automated Testing (by writing unit tests).
 
-## takeUntil.js
+## takeUtil.js
 
 Function keep collecting items from a provided array until the callback provided returns a truthy value.
 
@@ -12,3 +12,20 @@ Function keep collecting items from a provided array until the callback provided
   console.log(results1);
 ```
 Expected Output [ 1, 2, 5, 7, 2 ];
+
+## findKey.js
+
+Function takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+
+```js
+  findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2);
+```
+
+Expected Output "noma".
---
+++ findKey.js
@@ -0,0 +1,49 @@
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+// TEST CODE
+const check = String.fromCodePoint(0x2705);
+const wrong = String.fromCodePoint(0x274C);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${check} ${check} ${check}: ${actual} === ${expected}`);
+  } else {
+    console.log(`${wrong} ${wrong} ${wrong} : ${actual} !== ${expected}`);
+  }
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+ assertEqual(result, "noma");
+
+ const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "noma"
+
+ assertEqual(result1, "Akaleri");
